4d94517fa7f2d4d7d6261ff5e5b04a18968f681a
Your Name <sayankabery25@gmail.com>
2026-09-20 03:49:26 +0600

    Final route fix

--- backend/server.js
+++ backend/server.js
@@ -114,7 +114,7 @@ app.post('/api/posts/:id/comment', async (req, res) => {
   }
 });
 
-app.get('(.*)', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
